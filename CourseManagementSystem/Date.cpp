48e7a0e60c06e10be56d424fbd5de815cbcd0f5f
lephatminh <22125056@student.hcmus.edu.vn>
2023-04-07 09:37:33 +0700

    commit này để fix những bug Công Thành tạo ra :angry:
    + move variables to private mode
    + typo
    + bring functions to date.cpp

--- CourseManagementSystem/Date.cpp
+++ CourseManagementSystem/Date.cpp
@@ -1,3 +1,15 @@
 #include "Universal.h"
 date::date(int Day, int Month, int Year) :day(Day), month(Month), year(Year) {}
 date::date(){}
+int date::getDay()
+{
+	return day;
+}
+int date::getMonth()
+{
+	return month;
+}
+int date::getYear()
+{
+	return year;
+}
--- CourseManagementSystem/Student.h
+++ CourseManagementSystem/Student.h
@@ -3,25 +3,26 @@
 #include "Date.h"
 using namespace std;
 
-//class student
-//{
-//public:
-////	student(int No, string id, string firstName, string lastName, bool gender, date dob, string socialId, student* pointer);
-//	int no;
-//	string id, firstName, lastName, socialId;
-//	bool gender;
-//	date dob;
-//	student* pNext = nullptr;
-//	student* pStudent = nullptr;
-//
-//	//Constructor
-//	student(int no, string id, string firstname, string lastname, bool gender, date dob, string socialId, student* pointer) :
-//		no(no), id(id), firstName(firstname), lastName(lastname), gender(gender),
-//		dob(dob), socialId(socialId), pNext(pointer) {}
-//	void addStudentto1stClass(student*& headS);
-//	void viewProfile(student*& headS);
-//	void deleteStudentList(student*& headS);
-//private: 
-//	student* headS = nullptr;
-//};
+class student
+{
+public:
+//	student(int No, string id, string firstName, string lastName, bool gender, date dob, string socialId, student* pointer);
+
+	student* pNext = nullptr;
+	student* pStudent = nullptr;
+
+	//Constructor
+	student(int no, string id, string firstname, string lastname, bool gender, int day, int month, int year, string socialId, student* pointer) :
+		no(no), id(id), firstName(firstname), lastName(lastname), gender(gender),
+		dob(day, month, year), socialId(socialId), pNext(pointer) {}
+	void addStudentto1stClass(student*& headS);
+	void viewProfile(student*& headS);
+	void deleteStudentList(student*& headS);
+private: 
+	int no;
+	string id, firstName, lastName, socialId;
+	bool gender;
+	date dob;
+	student* headS = nullptr;
+};
 
